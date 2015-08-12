42150a6bca9fc38d45b3f137dfd4a2435a1a0cbd
Bin Xia <quanbinn@126.com>
2015-08-11 19:12:00 -0700

    add template addBIMForm

--- healthygeek.js
+++ healthygeek.js
@@ -1,8 +1,7 @@
 HealthygeeksList = new Mongo.Collection('healthygeeks');
 
 if (Meteor.isClient) {
-
-  Template.body.events({
+  Template.addBIMForm.events({
     "submit form": function (event) {
       // Prevent default browser form submit
       event.preventDefault();
@@ -13,22 +12,22 @@ if (Meteor.isClient) {
 
       var yourBMIFloatNum = yourWeight / ((yourHeight / 100) * (yourHeight / 100));// Calculate your BMI(kg/m2)
       var yourBMI = yourBMIFloatNum.toFixed(1);     // Calculate your BMI round(1)
-    document.getElementById("yourHeightInfo").textContent = "你的身高是 " + yourHeight +".";  
-    document.getElementById("yourWeightInfo").textContent = "你的体重是 " + yourWeight +".";  
-    document.getElementById("yourBMIInfo").textContent = "你的BMI是 " + yourBMI +".";  
+      
+      document.getElementById("yourHeightInfo").textContent = "你的身高是 " + yourHeight +".";  
+      document.getElementById("yourWeightInfo").textContent = "你的体重是 " + yourWeight +".";  
+      document.getElementById("yourBMIInfo").textContent = "你的BMI是 " + yourBMI +".";  
 
-    // document.getElementById("yourBMIAssess").textContent = "";  
-
-	if (yourBMI < 18.5) {										 	 // output your BMI description according to Hong Kong's BMI recommendation
+      // output your BMI description according to Hong Kong's BMI recommendation
+	if (yourBMI < 18.5) {									
     	document.getElementById("yourBMIAssess").textContent = "你的体重过轻"; 
 	}else if (yourBMI >= 18.5 && yourBMI < 23.0) {
 	    document.getElementById("yourBMIAssess").textContent = "你的体重正常"; 
 	}else if (yourBMI >= 23.0 && yourBMI < 25.0) {
 	    document.getElementById("yourBMIAssess").textContent = "你的体重超重，有健康危险"; 
 	}else if (yourBMI >= 25.0 && yourBMI < 30.0) {
-		document.getElementById("yourBMIAssess").textContent = "你的体重超重，属于中度肥胖"; 
+	    document.getElementById("yourBMIAssess").textContent = "你的体重超重，属于中度肥胖"; 
 	}else{
-		document.getElementById("yourBMIAssess").textContent = "你的体重超重，属于严重肥胖"; 
+	    document.getElementById("yourBMIAssess").textContent = "你的体重超重，属于严重肥胖"; 
 	};
 
       // Insert a task into the collection
@@ -43,7 +42,6 @@ if (Meteor.isClient) {
       // event.target.yourWeight.value = "";
     }
   });
-
 }
 
 if (Meteor.isServer) {
