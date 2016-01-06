2e803d48091b6c06753093d91d09c827ca83e043
Bin Xia <quanbinn@126.com>
2016-01-05 17:17:19 -0800

    pubSub the foods' record

--- client/main.js
+++ client/main.js
@@ -1,1 +1,5 @@
+// Subscribe the unique user's BMI infos
 Meteor.subscribe('theBMIInfos');
+
+// Subscribe the foods' record
+Meteor.subscribe('theFoods');
--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,11 @@
+// Publish the user's own BMI infos and only those
+// who created their BMI infos and see his or her record.
 Meteor.publish('theBMIInfos', function() {
 	var currentUserId = this.userId;
 	return BMIInfos.find({createdBy: currentUserId});
+});
+
+// Publish the foods
+Meteor.publish('theFoods', function() {
+	return Foods.find();
 });
